54b42dc3d42c9d21c6e804925e683b8c4cde600b
Miked1991 <149683329+Miked1991@users.noreply.github.com>
2023-12-04 13:29:07 +0000

    third commit with service page fixed

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/index.html")
 def display_home():
   return render_template('index.html')
 
-@app.route("/work.html")
-def display_work():
-  return render_template('work.html')
 
 @app.route("/services.html")
 def display_service():
   return render_template('services.html')
 
+
+@app.route("/work.html")
+def display_work():
+  return render_template('work.html')
+
+
 if __name__ == "__main__":
-  app.run(host="0.0.0.0",debug=True)
+  app.run(host="0.0.0.0", debug=True)
